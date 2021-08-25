ca786a28f8bb3be67771c2078b9e2b5df90f35dd
Fritiof Rusck <fritiof@bjornssonrusck.se>
2021-08-25 20:48:03 +0200

    update readme

--- README.md
+++ README.md
@@ -3,8 +3,19 @@ A way to create, solve, save and load sudokus with rust.
 ```rs
 
 fn main() {
-    // Creates a new solved sudoku
     let sudoku_game = sudoku::new_solved();
+}
+
+```
+If you want to solve a sudoku, you can load it in and the call the solve method
+```rs
+
+fn main() {
+    let mut my_cool_sudoku = sudoku::Board::new_from_bytes(byte_arr).unwrap();
+
+    my_cool_sudoku.solve().unwrap();
+
+    println!("{}", my_cool_sudoku);
 }
 
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod sudoku;
+use std::fs;
+use std::fs::File;
+use std::io::Read;
+
+// https://www.reddit.com/r/rust/comments/dekpl5/how_to_read_binary_data_from_a_file_into_a_vecu8/
+fn get_file_as_byte_vec(filename: &String) -> Vec<u8> {
+    let mut f = File::open(&filename).expect("no file found");
+    let metadata = fs::metadata(&filename).expect("unable to read metadata");
+    let mut buffer = vec![0; metadata.len() as usize];
+    f.read(&mut buffer).expect("buffer overflow");
+
+    buffer
+}
 
 fn main() {
-    println!("{}", sudoku::Board::new_solved());
+    let v = get_file_as_byte_vec(&String::from("sudoku/hard.sudoku"));
 
+    let mut byte_arr: [u8; 81] = [0; 81];
+
+    for (i, val) in v.iter().enumerate() {
+        byte_arr[i] = *val;
+    }
+
+    let mut hardest_sudoku = sudoku::Board::new_from_bytes(byte_arr).unwrap();
+
+    hardest_sudoku.solve().unwrap();
+
+    println!("{}", hardest_sudoku);
 }
